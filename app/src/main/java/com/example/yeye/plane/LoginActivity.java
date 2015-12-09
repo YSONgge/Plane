3cbad01f1e61855536ce5d4826ed2cc1cadb0026
YSONgge <1045719777@qq.com>
2015-12-09 16:25:15 +0800

    Create a new project,finished activity and fragment

---
+++ app/src/main/java/com/example/yeye/plane/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.yeye.plane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private android.support.v7.app.ActionBar bar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayShowHomeEnabled(true);
+        bar.setHomeButtonEnabled(true);
+        bar.setTitle("登录");
+    }
+
+    /*
+   点击按钮结束当前界面
+    */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        //return super.onOptionsItemSelected(item);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/yeye/plane/PassengerActivity.java
@@ -0,0 +1,35 @@
+package com.example.yeye.plane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class PassengerActivity extends AppCompatActivity {
+
+    private android.support.v7.app.ActionBar bar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger);
+        bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayShowHomeEnabled(true);
+        bar.setHomeButtonEnabled(true);
+        bar.setTitle("乘客信息");
+    }
+
+    /*
+   点击按钮结束当前界面
+    */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        //return super.onOptionsItemSelected(item);
+        return true;
+    }
+}
