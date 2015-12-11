eb31a4bd6e579eca397f10ccda8abf53e76dcb87
YSONgge <1045719777@qq.com>
2015-12-11 12:00:43 +0800

    passenger,contact_person has changed

--- app/src/main/java/com/example/yeye/plane/activity/OrderTicketActivity.java
+++ app/src/main/java/com/example/yeye/plane/activity/OrderTicketActivity.java
@@ -1,15 +1,65 @@
 package com.example.yeye.plane.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.yeye.plane.R;
 
 public class OrderTicketActivity extends AppCompatActivity {
 
+
+    private ImageView imageView1, imageView2;
+    private TextView name1;
+    private EditText name2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_ticket);
+
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        name1 = (TextView) findViewById(R.id.textView22);
+        name2 = (EditText) findViewById(R.id.editText9);
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(OrderTicketActivity.this, PassengerActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(OrderTicketActivity.this, ContactPersonActivity.class);
+                startActivityForResult(i, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            if (requestCode == 0 && resultCode == 0) {
+                Bundle data1 = data.getExtras();
+                String passenger = data1.getString("name");
+                name1.setText(passenger);
+                System.out.println("passenger = " + passenger);
+
+            } else if (requestCode == 1 && resultCode == 1) {
+                Bundle data2 = data.getExtras();
+                String contact = data2.getString("name");
+                name2.setText(contact);
+                System.out.println("contact = " + contact);
+            }
+            //super.onActivityResult(requestCode, resultCode, data);
+
+        }
     }
 }
--- app/src/main/java/com/example/yeye/plane/activity/PassengerActivity.java
+++ app/src/main/java/com/example/yeye/plane/activity/PassengerActivity.java
@@ -1,21 +1,43 @@
 package com.example.yeye.plane.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.yeye.plane.R;
 
 public class PassengerActivity extends AppCompatActivity {
 
     private android.support.v7.app.ActionBar bar;
+    private Button button;
+    private EditText name, cardNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger);
+
+        button = (Button) findViewById(R.id.button);
+        name = (EditText) findViewById(R.id.editText);
+        cardNumber = (EditText) findViewById(R.id.editText2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                intent.putExtra("name", name.getText().toString());
+                PassengerActivity.this.setResult(0, intent);
+                PassengerActivity.this.finish();
+            }
+        });
+
+
         bar = getSupportActionBar();
-        bar.setDisplayHomeAsUpEnabled(true);
+         bar.setDisplayHomeAsUpEnabled(true);
         bar.setDisplayShowHomeEnabled(true);
         bar.setHomeButtonEnabled(true);
         bar.setTitle("乘客信息");
@@ -26,7 +48,7 @@ public class PassengerActivity extends AppCompatActivity {
     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 break;
